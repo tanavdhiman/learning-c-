47cf1e66b7e17e52f5ee0037e50714f933651689
tanavdhiman <82314070+tanavdhiman@users.noreply.github.com>
2021-04-11 19:38:57 +0530

    Basics - Refractor to Code blocks

---
+++ Basics/data_types.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+
+// Below is only defining memory values but not printing on display
+char grade ='A';
+string phase="dps";
+int age=11;
+double gpa=4.5;
+bool ismale=true;
+
+// who will make display output "COUT'!!!!!
+cout<<"my grades are "<<grade<<" and my school is "<<phase<<" my age is "<<age
+<<" this year my gpa was "<<gpa<<" and my gender is "<<(ismale?"Male":"Female")<<endl;
+
+}
--- Basics/helloworld.cpp
+++ Basics/helloworld.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 using namespace std;
 
-	
+
 int main()
-{ 
+{
+
 string phase="tanav";
 cout<<"hello "<<phase<<endl;
 phase="world";
 cout<<"hello "<<phase<<endl;
-}  
+}

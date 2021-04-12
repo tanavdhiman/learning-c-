a971a91c1363e2f17149140b53918e132a15fa3e
tanavdhiman <82314070+tanavdhiman@users.noreply.github.com>
2021-04-12 16:49:30 +0530

    Basics

--- Basics/addtion.cpp
+++ Basics/addtion.cpp
@@ -1,8 +1,33 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
-	
-int main()
-{ 
 
-}  
+int main()
+{
+cout<<40*50<<endl;
+cout<<40-50<<endl;
+cout<<50/5<<endl;
+cout<<50%6<<endl;
+cout<<4+5*10<<endl;
+cout<<(4+5)*10<<endl;
+int wnum=5;
+    double dnum=5.5;
+    cout<<wnum<<endl;
+    cout<<dnum<<endl;
+    wnum+=80;
+    cout<<wnum<<endl;
+    cout<<5.5+9<<endl;
+    cout<<10.0/3.0<<endl;
+cout<<pow(2,5)<<endl;
+cout<<sqrt(36)<<endl;
+cout<<round(4.3)<<endl;
+cout<<ceil(4.1)<<endl;
+cout<<floor(4.9)<<endl;
+cout<<fmax(3,9)<<endl;
+cout<<fmin(3,9)<<endl;
+
+
+
+
+}
---
+++ Basics/user_input.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+
+int main()
+{
+int age;
+
+cout<<"enter your age:";
+cin>>age;
+cout<<"you are "<<age<<" years old"<< endl;
+cin.get();
+string name;
+cout<<"enter your name :";
+getline(cin,name);
+cout<<"hello"<<name;
+
+
+
+
+}

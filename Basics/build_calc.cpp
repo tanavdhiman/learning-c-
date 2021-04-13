fdd7fe9dee285ea8a75b52738444ec9ec432ec5b
tanavdhiman <82314070+tanavdhiman@users.noreply.github.com>
2021-04-13 15:50:15 +0530

    Basics

---
+++ Basics/build_calc.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+ int num1,num2;
+ cout<<"enter first number: ";
+cin>> num1;
+cout<<"enter second number: ";
+cin>> num2;
+cout<<"the answer to your question is: "<<num1+num2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
---
+++ Basics/game.cpp
@@ -0,0 +1,31 @@
+
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+string colour,pluralnoun,celebrity;
+
+cout<<"enter a colour: ";
+getline(cin,colour);
+cout<<"enter a plural noun: ";
+getline(cin,pluralnoun);
+cout<<"enter a celebrity: ";
+getline(cin,celebrity);
+
+cout<<"roses are "<<colour<<endl;
+cout<<pluralnoun<< " are blue"<<endl;
+cout<<"I love "<<celebrity<<endl;
+
+
+
+
+
+
+
+
+
+
+
+}

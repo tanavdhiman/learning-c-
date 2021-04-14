256f11b059f9baf9f3340cde4133948dd72825e8
tanavdhiman <82314070+tanavdhiman@users.noreply.github.com>
2021-04-14 16:31:15 +0530

    Basics

---
+++ Basics/arrays.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+
+int main()
+{
+int luckynum[20]={1,2,3,38,39};
+luckynum[10]=100;
+cout <<luckynum[10];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+

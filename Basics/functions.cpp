ba78fa8b8f662600bae6f3133ff29863a2c98ee0
tanavdhiman <82314070+tanavdhiman@users.noreply.github.com>
2021-04-16 13:43:20 +0530

    Basics

---
+++ Basics/functions.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+void sayhi(string name, int age){
+cout<<"hello "<<name<<"you are "<<age<<" years old"<<endl;
+
+
+
+}
+
+int main()
+{
+
+sayhi("mike ",11);
+sayhi("tanav ",12);
+sayhi("steve ",15);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
---
+++ Basics/return_statement.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+double cube(double num){
+double result = num *num*num;
+return result;
+
+
+
+
+
+}
+
+int main()
+{
+
+double answer =cube(5.0);
+cout<<answer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
